f169f97e7341ab72d943dfee0438e769fb6ba538
Agaisha Smith <agaishasmith@gmail.com>
2023-03-25 22:42:28 +0600

    code

--- Program.cs
+++ Program.cs
@@ -2,33 +2,32 @@
  или равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 */
-
 Console.Clear();
+string[] array1 = new string[] {"hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
 
-int size = 12;
-int[] array = GetArray(size);
-PrintArray(array);
+PrintArray(array1);
+FindElement(array1, array2);
+PrintArray(array2);
 
-int[] GetArray(int size)
+void FindElement(string[] array1, string[] array2)
 {
-  int[] array = new int[size];
-  for (int i = 0; i < size; i++)
-  {
-    array[i] = new Random().Next(0, 10);
-  }
-  return array;
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
 
-void PrintArray(int[] array)
-{
-  Console.Write($"[{String.Join(", ", array)}]");
-  Console.WriteLine();
-}
-
-int[] NewArray(int[] array)
-int el = array[i];
-for (int i = 0; i < array.Lenght; i++)
-{
-    if (el <= 3)
-    return el
-}
